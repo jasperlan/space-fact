6c0344c259c7c9b1c553d55c741bbb691c8f1332
jasperlan <jasperlan1690@yahoo.com>
2018-05-28 12:07:13 -0700

    a small change

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const languageStrings = {
                 'The Moon is moving approximately 3.8 cm away from our planet every year.',
             ],
             SKILL_NAME: 'British Space Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
+            GET_FACT_MESSAGE: "Here's your JASPER fact: ",
             HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
             HELP_REPROMPT: 'What can I help you with?',
             STOP_MESSAGE: 'Goodbye!',
